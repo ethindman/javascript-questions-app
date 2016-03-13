d4031002d997a1246e708a03d6045a79738b22de
Elliott Hindman <portokaliajapan@gmail.com>
2016-03-13 14:12:32 +0900

    add some new questions

--- src/assets/js/studyBuddy.js
+++ src/assets/js/studyBuddy.js
@@ -1,12 +1,17 @@
-var FURL 		= "https://<<yourfirebaseurl>>.firebaseio.com/";
+toastr.options.closeButton = true;
+
+
+var FURL 		= "https://sbapp1173.firebaseio.com/";
 var ref 		= new Firebase(FURL);
 
 var user 		= ref.child("users/ethindman");
 var buddy 	= ref.child("buddies/ethindman/yT49wmHY");
-var topics 	= ref.child("topics/yT49wmHY");
+var topics 	= ref.child("topics");
 
-ref.on("value", function(data) {
-  console.log(data.val());
+topics.on("value", function(data) {
+  var data = data.val();
+  console.log(data);
+ 
 }, function (errorObject) {
   console.log("The read failed: " + errorObject.code);
 });
@@ -76,26 +81,38 @@ function StudyBuddy(name) {
 		"How old are you?",
 		"What do you do?",
 		"Are you a student?",
+		"Where do you work?",
+		"How long have you studied English?",
+		"What did you study in college?",
+		"What sports do you play?",
+		"What do you like to do for fun?",
 	];
 
 	var myInterests = [
 		"What is your hobby?", 
 		"Do you like to go to the movies?", 
-		"What's your favorite movie?"
+		"What's your favorite movie?",
+		"Do you play any sports?",
+		"Do you play any instruments?",
+		"What do you do in your free time?"
 	];
 
 	var myDay = [
 		"How was your day today?",
 		"What did you do today?",
 		"How was work?",
-		"What did you have for lunch?"
+		"What did you have for lunch?",
+		"What did you have for dinner?",
+		"Did you sleep well last night?"
 	];
 
 	var myFamily = [
 		"How many brothers and sisters do you have?",
 		"Where does your family live?",
 		"What's your brother's name?",
-		"What's your sister's name?"
+		"What's your sister's name?",
+		"Do you have any brothers or sisters?",
+		
 	];
 
 	questionBtn.addEventListener('click', function() {
@@ -107,18 +124,22 @@ function StudyBuddy(name) {
 			case 'myInfo':
 				sb.clear();
 				sb.add(myInfo);
+				toastr.success('Topic Added', 'Success!');
 				break;
 			case 'myInterests':
 				sb.clear();
 				sb.add(myInterests);
+				toastr.success('Topic Added', 'Success!');
 				break;
 			case 'myDay':
 				sb.clear();
 				sb.add(myDay);
+				toastr.success('Topic Added', 'Success!');
 				break;
 			case 'myFamily':
 				sb.clear();
 				sb.add(myFamily);
+				toastr.success('Topic Added', 'Success!');
 				break;
 		}
 		e.stopPropagation();
